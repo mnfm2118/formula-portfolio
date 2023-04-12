ab57f80825d5144892e52aa7a9758a736351b680
mnfm2118 <kirutemu@gmail.com>
2023-04-12 13:45:56 +0900

    add library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import EditJs from './EditJs.vue'
 
-Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(EditJs),
 }).$mount('#app')
+
+//Vue.config.productionTip = false
