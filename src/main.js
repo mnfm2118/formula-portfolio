92e7c81a6e9ea3365765e19ae204492352859170
mnfm2118 <kirutemu@gmail.com>
2023-08-09 17:27:16 +0900

    vuetify inst

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 // import EditJs from './components/EditJs.vue'
 import router from './router'
+
+import vuetify from './plugins/vuetify'
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
+
   //render: h => h(EditJs),
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ]
 })
 // module.exports = {
 //   publicPath: './'
